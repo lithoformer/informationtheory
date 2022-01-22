a79db75dcdbc7d75f307111e80d26970afd52614
zhiguang eric zhang <5649294+zhiguangcode@users.noreply.github.com>
2022-01-21 20:17:11 -0500

    Add files via upload

---
+++ nexuski1.m
@@ -0,0 +1,30 @@
+function x = nexuski0(input0)
+
+if nargin ~= 1
+
+    error('please enter a string');
+
+end
+
+size0 = size(input0);
+
+if size0(1) ~= 1
+
+    error ('please enter a string');
+
+end
+
+length0 = size0(2);
+
+rng('shuffle');
+
+a = 32;
+b = 127;
+
+string0 = (b-a).*rand(length0,1) + a;
+
+x = char(floor(string0));
+
+disp(x');
+
+end
